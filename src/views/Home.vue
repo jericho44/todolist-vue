baaf64f0710ec33d36e6b34ca82668df8de6f753
jericho44 <ricoding17@gmail.com>
2021-09-03 08:47:53 +0700

    Todo List Vue.js

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <h2>What I do Today?</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <div
+            class="tugas"
+            v-for="tugasSaya in tugas"
+            v-bind:key="tugasSaya.id"
+          >
+            <p>{{ tugasSaya.nama }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  data: function () {
+    return {
+      tugas: [
+        {
+          id: 1,
+          nama: "Megalodon",
+        },
+        {
+          id: 2,
+          nama: "Mengoding",
+        },
+        {
+          id: 3,
+          nama: "Menghosting",
+        },
+      ],
+    };
+  },
+  components: {},
+};
+</script>
---
+++ src/views/Profile.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <div class="d-flex">
+            <div class="flex-shrink-0">
+              <img
+                src="../assets/+62856-0453-374320210815_193303.jpg"
+                class="user-pic"
+              />
+            </div>
+            <div class="flex-grow-1 ms-3">
+              <h3>Heading</h3>
+              <p>
+                This is some content from a media component. You can replace
+                this with any content and adjust it as needed.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  name: "Profile",
+  components: {},
+};
+</script>
+
+<style scoped>
+.user-pic {
+  height: 120px;
+  width: 120px;
+  border-radius: 200%;
+}
+</style>
